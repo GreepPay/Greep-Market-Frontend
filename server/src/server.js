4a79501f9fcf774296e7f37ed99804be73206a9c
Ayomide Akinola <drayfocus@gmail.com>
2025-09-22 00:10:24 +0100

    fix: prepare frontend

---
+++ server/src/server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../../build')));
+
+
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../../build/index.html'));
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
